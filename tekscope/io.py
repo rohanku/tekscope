13a36683fb35db1822df54b444109b51010c27dc
rohanku <rohankumar@berkeley.edu>
2023-06-09 13:28:38 -0700

    fix IO typing hints

--- tekscope/io.py
+++ tekscope/io.py
@@ -5,11 +5,11 @@ Utilities for saving oscilloscope data to disk.
 import os
 import pickle
 
-def save_waveforms(waveforms: dict[str, [int]], out_path: str):
+def save_waveforms(waveforms, [int]], out_path: str):
     with open(out_path, "wb") as f:
         f.write(pickle.dumps(waveforms))
 
-def load_waveforms(in_path: str) -> dict[str, [int]]:
+def load_waveforms(in_path: str):
     with open(in_path, "rb") as f:
         return pickle.loads(f.read())
 
