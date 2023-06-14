c1dc3fd2902b8658502e92bbaa1f522d6d24f6e7
rohanku <rohankumar@berkeley.edu>
2023-06-14 14:27:04 -0700

    add additional test

--- tests/test_osc.py
+++ tests/test_osc.py
@@ -13,8 +13,19 @@ def test_acquire_analog_sequence():
     """
     Tests acquisition of a length 200 analog sequence.
     """
-    osc = Oscilloscope(host="169.254.8.194", port=4000)
+    osc = Oscilloscope(host="0.0.0.0", port=4000)
 
     seq = osc.acquire_analog_sequence(200, AnalogSource.CH1)
     plt.plot(seq)
     plt.show()
+
+
+@pytest.mark.skip(reason="testing framework unimplemented")
+def test_retrieve_waveform():
+    """
+    Tests acquisition of a length 200 analog sequence.
+    """
+    osc = Oscilloscope(host="0.0.0.0", port=4000)
+
+    seq = osc.retrieve_all_waveforms()
+    print(seq.waveform_dict)
